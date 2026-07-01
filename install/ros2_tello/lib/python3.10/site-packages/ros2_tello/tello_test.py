7ad2d45dc33c322feed3cbc7ef710a679d4a17ce
ErickLLGN <120322855+ErickLLGN@users.noreply.github.com>
2026-07-01 18:07:18 -0300

    Add files via upload

---
+++ install/ros2_tello/lib/python3.10/site-packages/ros2_tello/tello_test.py
@@ -0,0 +1,34 @@
+from djitellopy import Tello
+import time
+import Tello_sensors
+from tello_controller import TelloController    #vx sempre para frente
+                                                #vy sempe para os lados
+
+tello = Tello()
+tello.connect()
+
+sensors = Tello_sensors.TelloSensors(tello)
+controller = TelloController(tello)
+#sensors.acceleration()
+
+ba = tello.get_battery()
+print(f"Battery Level: {ba}")
+
+
+user = input()
+
+
+
+try:
+
+    if user == "1":
+        tello.takeoff()
+        
+        controller.pos_ctrl(x=100, y=0, z=0, yaw=0, vx=25, vy=0, vz=0, yaw_speed=0, threshold=10)
+        controller.pos_ctrl(x=0, y=0, z=0, yaw=180, vx=0, vy=0, vz=0, yaw_speed=25, threshold=2)
+        controller.pos_ctrl(x=0, y=0, z=0, yaw=0, vx=25, vy=0, vz=0, yaw_speed=0, threshold=10)
+        tello.land()
+
+
+except KeyboardInterrupt:
+    tello.land()
